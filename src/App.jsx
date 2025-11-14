26e2598031387adb50c6e0af4479fb857bd55215
David Robert <contact@david-robert.fr>
2025-11-14 10:48:53 +0100

    loop with map in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,42 @@ import './App.css'
 
 function App() {
 
-    // faux appel d'api : fetch sur le serveur pour récupérer
-    // le nom de la personne connecté
-    const authentifiedUser =
-        {
-            firstName: 'David',
-            lastName: 'Robert',
-            age: 35,
-            job: "developper"
-        }
-    ;
 
-    // faux appel d'api : fetch sur le serveur pour savoir
-    // si l'utilisateur est connecté
-    const isUserLoggedIn = false;
+    const articles = [
+        {
+            id: 1,
+            title: "Understanding React Hooks",
+            content: "React Hooks are functions that let you use state and other React features without writing a class.",
+        },
+        {
+            id: 2,
+            title: "A Guide to JavaScript Closures",
+            content: "A closure is the combination of a function and the lexical environment within which that function was declared.",
+        },
+        {
+            id: 3,
+            title: "CSS Grid Layout Tutorial",
+            content: "CSS Grid Layout is a two-dimensional layout system for the web that allows you to create complex layouts easily.",
+        }
+    ]
+
 
   return (
     <>
-        { isUserLoggedIn ?
-            <h2>{ authentifiedUser.firstName } { authentifiedUser.lastName }</h2>
-        :
-            <h2>Veuillez vous connecter</h2>
-        }
+
+        <section>
+
+            {articles.map((article) => {
+                return (
+                    <article>
+                        <h1>{article.title}</h1>
+                    </article>
+                )
+            })}
+
+        </section>
+
+
     </>
   )
 }
